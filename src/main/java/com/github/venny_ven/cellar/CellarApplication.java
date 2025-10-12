1d08893af395d4452ed87cac8185b8a9a951a5e9
Ven <70820375+venny-ven@users.noreply.github.com>
2025-10-12 16:25:12 -0500

    Initial commit

---
+++ src/main/java/com/github/venny_ven/cellar/CellarApplication.java
@@ -0,0 +1,13 @@
+package com.github.venny_ven.cellar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CellarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CellarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/venny_ven/cellar/CellarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.venny_ven.cellar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CellarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
