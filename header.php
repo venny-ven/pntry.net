4e70ca566814cd2215a9a8db43b54b7c6e37366d
Ven <70820375+venny-ven@users.noreply.github.com>
2025-10-28 03:43:04 -0500

    Rename to pntry.net

--- header.php
+++ header.php
@@ -2,10 +2,10 @@
 <html lang="en"> <!-- Indicate that this page is an english version -->
 <head>
     <meta charset="UTF-8"> <!-- Use modern character set -->
-    <title>Cellar</title>
+    <title>pntry.net</title>
 </head>
 <body style="background:MistyRose;">
-    <h1 style="text-align:center;">Cellar</h1>
+    <h1 style="text-align:left;">pntry.net</h1>
     <a href="index.php">Home</a> |
     <a href="inventory.php">Manage inventory</a> |
     <a href="add-ingredient.php">Add an ingredient to the database</a>
