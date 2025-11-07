1864112852dd2f95d116ea1006cecf0d50c67459
Ven <70820375+venny-ven@users.noreply.github.com>
2025-11-07 07:40:23 -0600

    Expand about.php page

--- about.php
+++ about.php
@@ -1,5 +1,13 @@
 <?php include "header.php"; ?>
 
-<p>Some information about who I am, what this site is about, and why I made it.</p>
+<p><b>Hello, I'm Ven!</b> In front of you is a personal project I'm working on to  help me learn various parts of web development and self-hosting. 
+<p>Pntry.net is a simple web application that I designed primarily for myself, but I want it to also be useful to others. 
+    There are a number of apps out there that perform the function of managing the kitchen inventory,
+    ranging from very simple to very complex, but none that I've tried strike the right balance of ease of use,
+    feature completeness, and freedom from paywalls, ads, and other promotional bloat.
+    Hence I decided to make my own. So far the process has been a lot of fun.</p>
+    
+<p>This web application is <b>free and open source</b>, and you can explore the source code on my <a href="https://github.com/venny-ven/pntry.net">GitHub page</a>. 
+    I hope you enjoy using this site. You can message me directly on Discord <b>@venny_ven</b>. Ciao!</p>
 
 <?php include "footer.php";
